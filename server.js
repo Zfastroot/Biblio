010cc8fa228030dfe2a7486bb5b01824d3f07015
Zfastroot <zakaziko1305@gmail.com>
2024-11-10 17:42:57 +0100

    Set Up the router

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-//import express
+// Import the Express module
 const express = require("express")
-// run express
+// Run Express by creating an instance of the Express application
 const app = express()
-const expressLayouts = require('express-ejs-layouts')
 
-app.set('view engine','ejs')
-app.set('views',__dirname + '/views')
-app.set('layout','layouts/layout')
+// Import the express-ejs-layouts module for layout support in EJS
+const expressLayouts = require('express-ejs-layouts')
+//Import our Router
+const indexRouter = require('./routes/index')
+// Set EJS as the templating engine for rendering views
+app.set('view engine', 'ejs')
+
+// Set the directory where the view templates (EJS files) are stored
+app.set('views', __dirname + '/views')
+
+// Define the default layout file for all views (layouts/layout.ejs)
+app.set('layout', 'layouts/layout')
+
+// Use express-ejs-layouts middleware to enable layout functionality
 app.use(expressLayouts)
+
+// Serve static files (like CSS, images, JS) from the "public" directory
 app.use(express.static('public'))
 
+//use the routes  
+app.use('/',indexRouter)
+
+// Start the server on the environment's specified port (process.env.Port) or default to port 3000
 app.listen(process.env.Port || 3000)
+
