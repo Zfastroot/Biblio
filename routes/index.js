add4ed408de713937d4be197cdf262c692d873a3
Zfastroot <zakaziko1305@gmail.com>
2024-11-10 17:53:53 +0100

    configue the views and test if the layouts working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+// Import the Express module
 const express = require('express')
 
+// Create a new router object using Express Router
 const router = express.Router()
 
-router.get('/' , (req,res)=>{
-    res.send('hello World')
+// Define a GET route for the root URL ('/') and send a response when it's accessed
+router.get('/', (req, res) => {
+    // Send 'hello World' as the response to the client
+    res.render('index')
 })
 
+// Export the router so it can be used in other parts of the application
 module.exports = router
